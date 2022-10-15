2ac1401c70e4e2e01019e27522489f1cb8391499
Dewansh Khandelwal <dewansh3255@gmail.com>
2022-10-15 11:13:21 +0530

    Added the required functions

--- Lights_Out/comment/forms.py
+++ Lights_Out/comment/forms.py
@@ -4,6 +4,21 @@ from comment.models import Comment
 class CommentForm(forms.ModelForm):
 	body = forms.CharField(widget=forms.Textarea(attrs={'class': 'materialize-textarea'}), required=False)
 
-	class Meta:
-		model = Comment
-		fields = ('body',)
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes")
+
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11))
+print(mytripler(11))
